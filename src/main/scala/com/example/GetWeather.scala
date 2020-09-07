b84a2bdd0b6bd3ce25dd8a9ff32d4b51eb147948
Iriko0606 <eriko.i0606@gmail.com>
2020-09-07 23:16:01 +0900

    不要ファイル削除

--- src/main/scala/com/example/GetWeather.scala
+++ src/main/scala/com/example/GetWeather.scala
@@ -4,33 +4,14 @@ import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
-import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.unmarshalling.Unmarshal
 
 import scala.concurrent.Future
-import scala.util.{Failure, Success}
 
-//#main-class
-object QuickstartApp {
-  //#start-http-server
-  private def startHttpServer(routes: Route)(implicit system: ActorSystem[_]): Unit = {
-    // Akka HTTP still needs a classic ActorSystem to start
-    import system.executionContext
-
-    val futureBinding = Http().newServerAt("localhost", 8080).bind(routes)
-    futureBinding.onComplete {
-      case Success(binding) =>
-        val address = binding.localAddress
-        system.log.info("Server online at http://{}:{}/", address.getHostString, address.getPort)
-      case Failure(ex) =>
-        system.log.error("Failed to bind HTTP endpoint, terminating system", ex)
-        system.terminate()
-    }
-  }
+object GetWeather {
   //#start-http-server
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem(Behaviors.empty, "SingleRequest")
-    // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.executionContext
 
     val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "https://api.openweathermap.org/data/2.5/onecall?lat=35.681236&lon=139.767125&units=metric&lang=ja&appid=83d025aeaf06051689e0a2a8a892b576"))
@@ -40,4 +21,3 @@ object QuickstartApp {
       } yield println(bodyText)
   }
 }
-//#main-class
