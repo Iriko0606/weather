0c77d8f298e6694a4b5704e3dfb33d250865152a
s04802 <irinaka_eriko@cyberagent.co.jp>
2020-10-30 14:43:01 +0900

    ファイル名に日付を追加

--- src/main/scala/com/example/GetWeather.scala
+++ src/main/scala/com/example/GetWeather.scala
@@ -1,21 +1,30 @@
 package com.example
+
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.http.scaladsl.unmarshalling.Unmarshal
-import cats._
+
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
-object GetWeather {
+import java.io.{PrintWriter}
+import java.util.Date
+
+object GetWeather{
+
   val city = "Kawagoe"
-  val key = "83d025aeaf06051689e0a2a8a892b576"
+  val key = "apikey"
   val url = s"https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}"
+  val date = "%tF" format new Date()
+
   implicit val system = ActorSystem(Behaviors.empty, "SingleRequest")
   implicit val executionContext = system.executionContext
   def main(args: Array[String]): Unit = {
     val weather = Await.result(getWeather(url), Duration.Inf)
-    println(weather)
+
+    writeJsonFile(weather)
+    awsS3.putObjects("weatherinfo", s"weather${date}.json", "weather")
   }
   def getWeather(url: String): Future[String] = {
     val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = url))
@@ -24,4 +33,11 @@ object GetWeather {
       bodyText <- Unmarshal(res.entity).to[String]
     } yield bodyText
   }
+
+  def writeJsonFile(contents: String): Unit = {
+    val file = new PrintWriter(s"weather${date}.json")
+    file.write(contents)
+    file.close()
+  }
+
 }
